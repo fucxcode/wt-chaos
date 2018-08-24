ceeadb4eaf37a9cf232889fa7b52b0d0a6cd8deb
Shaun Xu <jfarrio@gmail.com>
2018-08-24 16:03:09 +0800

    #172104

---
+++ test/container.default.spec.ts
@@ -0,0 +1,113 @@
+import { assert } from "chai";
+import * as uuid from "node-uuid";
+import * as _ from "../src/utilities";
+
+import { IContainer, lifecycles } from "../src/container/i-container";
+import { registerContainer, clearContainers, resolveContainer, setDefaultContainer } from "../src/container/container";
+import { inject, injectable } from "../src/container/decorators";
+import { BypassActivationHandler } from "../src/container/activation-handlers/bypass-activation-handler";
+
+describe(`default container`, () => {
+
+    beforeEach(() => {
+        clearContainers();
+    });
+
+    it(`register 1st container, set default. inject and injectable should use default container`, () => {
+        const key = Symbol("default");
+        const container = registerContainer(key, new BypassActivationHandler());
+        setDefaultContainer(key);
+
+        @injectable()
+        // @ts-ignore
+        class InjectableClass {
+            private _id: string;
+            public get id(): string {
+                return this._id;
+            }
+            constructor(id: string = uuid.v4()) {
+                this._id = id;
+            }
+        }
+
+        @injectable()
+        // @ts-ignore
+        class InjectedClass {
+            @inject()
+            // @ts-ignore
+            private _injected: InjectableClass;
+            public get injected(): InjectableClass {
+                return this._injected;
+            }
+            constructor() {
+            }
+        }
+
+        const instance = container.resolve<InjectedClass>(InjectedClass);
+        assert.ok(instance);
+        assert.ok(instance.injected);
+    });
+
+    it(`register 1st container, no default. inject and injectable should use this container as default`, () => {
+        const key = Symbol("default");
+        const container = registerContainer(key, new BypassActivationHandler());
+
+        @injectable()
+        // @ts-ignore
+        class InjectableClass {
+            private _id: string;
+            public get id(): string {
+                return this._id;
+            }
+            constructor(id: string = uuid.v4()) {
+                this._id = id;
+            }
+        }
+
+        @injectable()
+        // @ts-ignore
+        class InjectedClass {
+            @inject()
+            // @ts-ignore
+            private _injected: InjectableClass;
+            public get injected(): InjectableClass {
+                return this._injected;
+            }
+            constructor() {
+            }
+        }
+
+        const instance = container.resolve<InjectedClass>(InjectedClass);
+        assert.ok(instance);
+        assert.ok(instance.injected);
+    });
+
+    it(`no register container. inject and injectable should raise exception`, () => {
+        assert.throw(() => {
+            @injectable()
+            // @ts-ignore
+            class InjectableClass {
+                private _id: string;
+                public get id(): string {
+                    return this._id;
+                }
+                constructor(id: string = uuid.v4()) {
+                    this._id = id;
+                }
+            }
+
+            @injectable()
+            // @ts-ignore
+            class InjectedClass {
+                @inject()
+                // @ts-ignore
+                private _injected: InjectableClass;
+                public get injected(): InjectableClass {
+                    return this._injected;
+                }
+                constructor() {
+                }
+            }
+        });
+    });
+});

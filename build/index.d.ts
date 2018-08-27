12db711ab4cd296c6d838291f99d1506c249c38c
Shaun Xu <jfarrio@gmail.com>
2018-08-27 18:23:05 +0800

    .

--- build/index.d.ts
+++ build/index.d.ts
@@ -3,7 +3,7 @@ import * as constants from "./constants";
 import { WTError, WTErrorResponse, code } from "./errors";
 import * as container from "./container";
 import * as cache from "./cache";
-import { Redis } from "./redis";
+import * as redis from "./redis";
 import * as serializer from "./serializer";
-export { _, constants, container, cache, WTError, WTErrorResponse, code, Redis, serializer };
+export { _, constants, container, cache, WTError, WTErrorResponse, code, redis, serializer };
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as constants from "./constants";
 import { WTError, WTErrorResponse, code } from "./errors";
 import * as container from "./container";
 import * as cache from "./cache";
-import { Redis } from "./redis";
+import * as redis from "./redis";
 import * as serializer from "./serializer";
 
 export {
@@ -14,7 +14,7 @@ export {
     WTError,
     WTErrorResponse,
     code,
-    Redis,
+    redis,
     serializer
 };
 

2b6d1efb7d189a5b4f8b0e085862b50639fe7a65
Shaun Xu <jfarrio@gmail.com>
2018-11-06 11:36:31 +0800

    #186443

--- build/container/activation-handlers/index.js
+++ build/container/activation-handlers/index.js
@@ -5,5 +5,5 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./bypass-activation-handler"));
 __export(require("./trace-decorator"));
-// export * from "./tracing-activation-handler";
+__export(require("./tracing-activation-handler"));
 //# sourceMappingURL=index.js.map
---
+++ test/container-activity-tracing.spec.ts
@@ -0,0 +1,13 @@
+import { IContainer } from "../src/container/i-container";
+import { clearContainers, registerContainer, ActivityTracingActivationHandler } from "../src/container";
+
+// describe(`container`, () => {
+
+//     let container: IContainer;
+
+//     beforeEach(() => {
+//         clearContainers();
+//         container = registerContainer(Symbol(), new ActivityTracingActivationHandler();
+//     });
+
+// });

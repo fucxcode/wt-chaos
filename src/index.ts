97cefe90b725ae5fa0e83e960f33c5a392d5890a
Shaun Xu <jfarrio@gmail.com>
2018-11-14 14:55:16 +0800

    export i18n

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import * as serializer from "./serializer";
 import * as router from "./router";
 import * as repository from "./repository";
 import * as facade from "./facade";
+import * as i18n from "./i18n";
 
 export {
     _,
@@ -19,6 +20,7 @@ export {
     serializer,
     router,
     repository,
-    facade
+    facade,
+    i18n
 };
 

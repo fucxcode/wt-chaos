179d2509beda196f7e035aff0e0241f4434f21d4
Shaun Xu <jfarrio@gmail.com>
2018-12-11 15:38:20 +0800

    npm update tp latest version

--- src/serializer/bson-serializer.ts
+++ src/serializer/bson-serializer.ts
@@ -1,20 +1,14 @@
 import { Serializer } from "./serializer";
-import { BSON } from "bson";
+import BSON from "bson";
 
 class BSONSerializer implements Serializer<Buffer> {
 
-    private _bson: BSON;
-
-    constructor() {
-        this._bson = new BSON();
-    }
-
     public serialize<T>(value: T): Buffer {
-        return this._bson.serialize(value);
+        return (BSON as any).serialize(value);
     }
     
     public deserialize<T>(buf?: Buffer | undefined): T | undefined {
-        return buf && this._bson.deserialize(buf);
+        return buf && (BSON as any).deserialize(buf);
     }
 
 }
